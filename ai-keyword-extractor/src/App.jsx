fcfe83ffa0d37d1fb56ee57f46663efe22edba3b
krishydty <35625095+krishydty@users.noreply.github.com>
2023-06-14 00:12:06 +0100

    Completed code

--- ai-keyword-extractor/src/App.jsx
+++ ai-keyword-extractor/src/App.jsx
@@ -1,21 +1,56 @@
-
+import { useState } from 'react';
 import { Container, Box } from '@chakra-ui/react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import TextInput from './components/TextInput';
+
+
 const App = () => {
+  const [keywords, setKeywords] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const extractKeywords = async (text) => {
+    setLoading(true);
+    setIsOpen(true);
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt:
+          'Extract keywords from this text. Make the first letter of every word uppercase and separate with commas:\n\n' +
+          text +
+          '',
+        temperature: 0.5,
+        max_tokens: 60,
+        top_p: 1.0,
+        frequency_penalty: 0.8,
+        presence_penalty: 0.0,
+      }),
+    };
+    const response = await fetch(
+      import.meta.env.VITE_OPENAI_API_URL,
+      options
+    );
+    const json = await response.json();
+
+  };
+
   return (
-    <Box bg='blue.900' color='whiteAlpha.600' height='100vh' paddingTop={130}>
+    <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
       <Container maxW='3xl' centerContent>
         <Header />
-        <TextInput />
+        <TextInput extractKeywords={extractKeywords} />
         <Footer />
-        
       </Container>
-    
+      
     </Box>
   );
 };
 
 export default App;
-
